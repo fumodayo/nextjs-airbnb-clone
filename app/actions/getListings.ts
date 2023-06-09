36338d2c1334895204cc4668555dc313355e7319
thairyo <sonthai1310.works@gmail.com>
2023-06-09 19:14:54 +0700

    feat: Properties screen (Loading your listing with server component)

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -1,8 +1,21 @@
 import prisma from "@/app/libs/prismadb";
 
-export default async function getListings() {
+export interface IListingsParams {
+  userId?: string;
+}
+
+export default async function getListings(params: IListingsParams) {
   try {
+    const { userId } = params;
+
+    let query: any = {};
+
+    if (userId) {
+      query.userId = userId;
+    }
+
     const listings = await prisma.listing.findMany({
+      where: query,
       orderBy: {
         createdAt: "desc",
       },
@@ -14,7 +27,6 @@ export default async function getListings() {
     }));
 
     return safeListing;
-    
   } catch (error: any) {
     throw new Error(error);
   }
